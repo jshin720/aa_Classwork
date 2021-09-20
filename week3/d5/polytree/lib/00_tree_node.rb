3a7617f7e7dbc57c3b7619b276580044609d7b29
Jshin720 <jeffshin720@gmail.com>
2021-09-20 17:18:22 -0400

    Working on parent= method

--- week3/d5/polytree/lib/00_tree_node.rb
+++ week3/d5/polytree/lib/00_tree_node.rb
@@ -8,12 +8,29 @@ class PolyTreeNode
 
     def parent=(node)
         #node== parent
-        if node!=nil
-            @parent =node
-            node.children.push(self) if !node.children.include?(self)
+        
+        if node == nil
+            @parent.children.reject { |child_node| child_node == self } 
+            @parent = nil
         else
-            return nil
+            if @parent != nil
+                prev_parent = @parent
+                prev_parent.children.reject { |child_node| child_node == self }
+            end
+            node.children.push(self) if !node.children.include?(self)
+            @parent = node
         end
+        
+        # if node!=nil
+        #     @parent =node
+        #     node.children.push(self) if !node.children.include?(self)
+        # else
+        #     prev_parent = @parent
+        #     prev_parent.children.reject { |child_node| child_node == self }
+        #      @parent = node
+        #     node.children.push(self)
+        # end
+
     end
 
 end
