972d65820621d2126b589a703772ad9cba6f1178
Lyhour Lay <lyhourlay1@gmail.com>
2021-09-20 11:28:50 -0700

    initialize method

--- week3/d5/polytree/lib/00_tree_node.rb
+++ week3/d5/polytree/lib/00_tree_node.rb
@@ -1,3 +1,19 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
+    def initialize(value)
+        @parent=nil
+        @children =[]
+        @value=value
+    end
+
+    def parent=(node)
+        #node== parent
+        if node!=nil
+            @parent =node
+            node.children.push(self) if !node.children.include?(self)
+        else
+            return nil
+        end
+    end
 
 end
