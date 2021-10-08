cc7c024e4d76d9cad4c75f9ffc90b2665cc2fb80
Jshin720 <jeffshin720@gmail.com>
2021-10-08 19:03:21 -0400

    working on controller methods

--- week6/d5/cats/app/controllers/cats_controller.rb
+++ week6/d5/cats/app/controllers/cats_controller.rb
@@ -10,4 +10,14 @@ class CatsController < ApplicationController
     render :show
   end
 
+  def new 
+    @cats = Cat.new(cats_params)
+    render :new
+  end
+
+  def cats_params
+    params.require(:cat).permit(:birth_date, :name, :color, :sex, :description, :age)
+
+  end
+
 end
