cc94b5acd1ed34acd486b12fefd4f3810e19a657
Grayson Poon <grayson.a.poon@gmail.com>
2021-10-11 15:14:50 -0700

    Create password setter, is_password?, and class methods

--- week7/d1/99cats/app/models/user.rb
+++ week7/d1/99cats/app/models/user.rb
@@ -1,16 +1,36 @@
 class User < ApplicationRecord
+  before_validation :ensure_session_token
+  # after_initialize :ensure_session_token
+  
   validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
   validates :password, length: {minimum: 6}, allow_nil: true
+  
   attr_reader :password
 
-  before_validation :ensure_session_token
-  # after_initialize :ensure_session_token
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username) # check username column in table for input username
+    if user && user.is_password?(password)
+      return user
+    else
+      return nil
+    end
+  end
 
   def reset_session_token!
     self.session_token = SecureRandom::urlsafe_base64
     self.save!
     return self.session_token
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password) # creates a BCrypt string object that is stored in db
+    @password = password
+  end
+
+  def is_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
   end
 
 end
