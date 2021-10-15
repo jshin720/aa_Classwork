0d1d24447b51f70ec51592bc76882b1aec6e36d1
Julian Erville <jerville@gmail.com>
2021-10-15 17:17:06 -0400

    begin auth setup

---
+++ week7/d5/redditClone/app/models/user.rb
@@ -0,0 +1,42 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  attr_reader :password
+  after_initialize :ensure_token
+  validates :name, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: {minimum: 6}, allow_nil: true
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    user.is_valid_password?(password) ? user : nil
+  end
+  
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.new(password)
+  end
+
+  def is_valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+  end
+
+  def ensure_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+end
---
+++ week7/d5/redditClone/test/models/user_test.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
