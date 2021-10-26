10c81c43c19132ae3908430f080a715a03f1479a
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-10-26 15:07:14 -0700

    progress [ar]

--- w9/d2/browserttt/src/ttt-view.js
+++ w9/d2/browserttt/src/ttt-view.js
@@ -2,20 +2,24 @@ class View {
   constructor(game, el) {}
 
   setupBoard() {
-    const ttt = document.getElementsByClassName('ttt')
-    let grid = document.createElement('div')
-    let rows = document.getElementsByClassName('gridRow')
-    for (let i = 0; i < 3; i++) {
-      let row = document.createElement('ul');
-      grid.appendChild(row).className = 'gridRow'
+    // // const ttt = document.getElementsByClassName('ttt')
+    // let grid = document.createElement('div')
+    // // let rows = document.getElementsByClassName('gridRow')
+    // for (let i = 0; i < 3; i++) {
+    //   let row = document.createElement('ul');
+    //   grid.appendChild(row).className = 'gridRow'
 
-      for (let j = 0; j < 3; j++) {
-      let cell = document.createElement('li')
-      row.appendChild(cell).className = `${i, j}` 
-      }
-    }
-    
-    
+    //   for (let j = 0; j < 3; j++) {
+    //   let cell = document.createElement('li')
+    //   row.appendChild(cell).className = 'cell'
+    //   debugger
+    //     // `${i, j}`
+    //   }
+    // }
+
+    game.board.grid.forEach((cell) => {
+      
+    })
   }
   
   bindEvents() {}
