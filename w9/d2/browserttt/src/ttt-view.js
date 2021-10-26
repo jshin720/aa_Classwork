927604121430214718b09e661b4ddbf55b33e239
Jshin720 <jeffshin720@gmail.com>
2021-10-26 17:15:49 -0400

    Starting setupBoard

---
+++ w9/d2/browserttt/src/ttt-view.js
@@ -0,0 +1,31 @@
+class View {
+  constructor(game, el) {}
+
+  setupBoard() {
+    const ttt = document.getElementsByClassName('ttt')
+    let grid = document.createElement('div')
+    let rows = document.getElementsByClassName('gridRow')
+    for (let i = 0; i < 3; i++) {
+      let row = document.createElement('ul');
+      grid.appendChild(row).className = 'gridRow'
+
+      for (let j = 0; j < 3; j++) {
+      let cell = document.createElement('li')
+      row.appendChild(cell).className = `${i, j}` 
+      }
+    }
+    
+    
+  }
+  
+  bindEvents() {}
+
+  handleClick(e) {}
+
+  makeMove(square) {}
+
+}
+
+
+
+module.exports = View;
